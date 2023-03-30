00c329a064fb0132a3062b7ca8f9f44e55b4c8dc
Akash NP <akashnp1998@gmail.com>
2023-03-30 21:53:21 +0530

    feat: update data fetcher from stock axios to new custom axios instance

--- src/lib/employees.jsx
+++ src/lib/employees.jsx
@@ -1,20 +1,19 @@
-import axios from "axios"
-
 import { getCookie } from "./utils"
+import api from "./axios";
 
 export async function getEmployees() {
     const data = [];
     try {
         const accessToken = getCookie("access");
-        const response = await axios.get("http://localhost:8000/api/employee/", {
+        const response = await api.get("http://localhost:8000/api/employee/", {
             headers: {
-                Authorization: `Token ${accessToken}`
+                Authorization: `Bearer ${accessToken}`
             }
         })
         
         data.push(...response.data.results)
     } catch(error) {
-        console.log(error)
+        // console.log(error)
     }
     return data
 }
