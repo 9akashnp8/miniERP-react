fc76a0809eb9bf0f59032ca4854cdf291b080547
Akash NP <akashnp1998@gmail.com>
2023-04-07 11:10:48 +0530

    chore: move custom styled components to common theme module, improve DRY

--- src/lib/theme.js
+++ src/lib/theme.js
@@ -1,4 +1,5 @@
 import TableCell from '@mui/material/TableCell';
+import InputBase from '@mui/material/InputBase';
 
 import { createTheme } from "@mui/material/styles";
 import { styled, alpha } from '@mui/material/styles';
@@ -25,6 +26,47 @@ export const darkTheme = createTheme({
 });
 
 // Custom styled components
+export const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+        width: 'auto',
+    },
+}));
+
+export const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
+export const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+                width: '20ch',
+            },
+        },
+    },
+}));
+
 export const StyledTableCell = styled(TableCell)(({ theme }) => ({
     fontWeight: 'bold'
 }))
