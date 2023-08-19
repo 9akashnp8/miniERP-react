428f346ff7454603102f291c738f748bfc138aa0
Akash NP <akashnp1998@gmail.com>
2023-08-19 08:03:38 +0400

    feat: replace ts-jest with swc/jest
    SWC is drop-in alternative, written in Rust and super fast. Upto
    More than 10x improvement

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,21 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'jsdom',
   transform: {
     // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
     // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.tsx?$': [
+      '@swc/jest',
+      {
+        jsc: {
+          transform: {
+            react: {
+              runtime: 'automatic',
+            },
+          },
+        },
+      },
+    ],
   },
   transformIgnorePatterns: ['<rootDir>/node_modules/'],
   extensionsToTreatAsEsm: ['.ts'],
