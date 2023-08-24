071b6fc6d9c50891d81f6077832e4422d9e11280
Akash NP <akashnp1998@gmail.com>
2023-08-24 08:47:50 +0400

    chore: test redirection on delete functionality

--- src/features/common/components/Delete/index.spec.tsx
+++ src/features/common/components/Delete/index.spec.tsx
@@ -1,14 +1,32 @@
 import "cross-fetch/polyfill";
+import "@testing-library/jest-dom";
 import { render, screen } from "../../utils/test-utils";
 import userEvent from "@testing-library/user-event";
-import "@testing-library/jest-dom";
+import { Route, Routes } from 'react-router-dom'
 
 import Delete from ".";
 
+beforeEach(() => {
+  const App = () => (
+    <div>
+      <Delete employeeId="" />
+      <Routes>
+        <Route path="/" element={<div>Home Page</div>} />
+        <Route path="/employee" element={<div>Employee Detail Page</div>} />
+      </Routes>
+    </div>
+  )
+  render(<App />)
+})
+
 test("confirmation message is displayed on click", async () => {
-  render(<Delete employeeId="2" />);
-
   await userEvent.click(screen.getByText("Delete"));
-
   expect(screen.getByRole("menuitem")).toHaveTextContent("Confirm?");
 });
+
+test("redirection works on delete",async () => {
+  // screen.debug();
+  await userEvent.click(screen.getByText("Delete"));
+  await userEvent.click(screen.getByText("Confirm?"));
+  expect(screen.getByText('Employee Detail Page2')).toBeInTheDocument();
+})
