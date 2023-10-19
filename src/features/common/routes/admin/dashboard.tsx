66876a5c001c2fa43c171394210ff33e7841f1dc
Akash NP <akashnp1998@gmail.com>
2023-10-19 20:24:39 +0400

    feat: add icons to setting cards

--- src/features/common/routes/admin/dashboard.tsx
+++ src/features/common/routes/admin/dashboard.tsx
@@ -1,42 +1,58 @@
 // Material UI Components
+import Divider from '@mui/material/Divider';
+import LanIcon from '@mui/icons-material/Lan';
 import Typography from "@mui/material/Typography"
+import LaptopIcon from '@mui/icons-material/Laptop';
+import PeopleIcon from '@mui/icons-material/People';
+import LocationCityIcon from '@mui/icons-material/LocationCity';
+import CorporateFareIcon from '@mui/icons-material/CorporateFare';
+import SettingsApplicationsIcon from '@mui/icons-material/SettingsApplications';
 
 // Internal UI Components
-import Card from "../../../employee/components/Card"
 import Grid from "../../components/Grid"
+import Card from "../../../employee/components/Card"
 
 const adminCards = [
     {
         title: "Departments",
-        url: "/admin/departments"
+        url: "/admin/departments",
+        icon: <CorporateFareIcon />
     },
     {
         title: "Designations",
-        url: "/admin/designations"
+        url: "/admin/designations",
+        icon: <LanIcon />
+
     },
     {
         title: "Locations",
-        url: "/admin/branches"
+        url: "/admin/branches",
+        icon: <LocationCityIcon />
     },
     {
-        title: "Settings",
-        url: "/admin/settings"
+        title: "Employee Settings",
+        url: "/admin/employee-settings",
+        icon: <SettingsApplicationsIcon />
     },
     {
         title: "Buildings",
-        url: "/admin/building"
+        url: "/admin/building",
+        icon: <LocationCityIcon />
     },
     {
         title: "Laptop Brands",
-        url: "/admin/laptop-brands"
+        url: "/admin/laptop-brands",
+        icon: <LaptopIcon />
     },
     {
-        title: "Settings",
-        url: "/admin/settings"
+        title: "Hardware Settings",
+        url: "/admin/hardware-settings",
+        icon: <SettingsApplicationsIcon />
     },
     {
         title: "Users",
-        url: "/admin/users"
+        url: "/admin/users",
+        icon: <PeopleIcon />
     },
 ]
 
@@ -46,13 +62,15 @@ export default function AdminDasboardRoute() {
             <Typography variant="h4" component="h1" gutterBottom>
                 Admin Panel
             </Typography>
-            <Grid container spacing={3}>
+            <Divider />
+            <Grid container spacing={3} pt={3}>
                 {adminCards.map((cardDetail) => {
                     return (
                         <Grid item xs={4}>
                            <Card
                                 title={cardDetail.title}
                                 url={cardDetail.url}
+                                icon={cardDetail.icon}
                             />
                         </ Grid>
                     )
