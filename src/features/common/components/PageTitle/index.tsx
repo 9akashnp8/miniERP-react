2d9c83512e33446fa04e7d87f03102107fd1b4bc
Akash NP <akashnp1998@gmail.com>
2023-10-22 11:09:24 +0400

    chore: minor fixes

--- src/features/common/components/PageTitle/index.tsx
+++ src/features/common/components/PageTitle/index.tsx
@@ -4,7 +4,7 @@ import { FCWithChildren } from "../../../../types/common"
 
 export default function PageTitle({ children }: FCWithChildren) {
     return (
-        <Typography variant="h4" component="h1" gutterBottom>
+        <Typography variant="h4" component="h1">
             {children}
         </Typography>
     )
